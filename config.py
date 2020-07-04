2e1909d0edfc12ad95b909ae6005b016cc8ad31b
ScorP14 <scorpeon14@mail.ru>
2020-07-04 23:41:50 +0800

    re

--- config.py
+++ config.py
@@ -2,7 +2,6 @@ import os
 API_TOKEN = '802097136:AAGIr2s_5ZbiCSyV0qsZmdihsLjGHQRiQuk'
 PROXY_URL = 'socks5://104.248.63.15:30588'
 ADMIN = 468933460
-IRA = 956608146
 
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
