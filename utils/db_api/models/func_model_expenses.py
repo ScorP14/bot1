63d0e7f878cdbe36d887e1b07c849b6f03e11998
ScorP14 <scorpeon14@mail.ru>
2020-07-12 15:50:36 +0800

    re

--- utils/db_api/models/func_model_expenses.py
+++ utils/db_api/models/func_model_expenses.py
@@ -1,5 +1,5 @@
 from utils.db_api.models.models import User, Categories, Expenses
-from utils.db_api.utility_for_db import parse_text_for_expenses
+from utils.db_api.utility_for_db import parse_text_for_expenses, get_one_day_data_for_db
 
 
 def add_expenses(id_tg, mes):
@@ -20,3 +20,19 @@ def select_all():
     exp = Expenses.select()
     for i in exp:
         print(i)
+
+
+def test():
+    """Считает среднее за день, неделю, месяц, год"""
+    start, end = get_one_day_data_for_db(2020, 8, 10)
+    print(start, '-----------', end)
+    sel = Expenses\
+        .select(Expenses.price, Expenses.date_add)\
+        .where(Expenses.date_add.between(start, end))\
+        .order_by(Expenses.date_add)
+    temp = 0.0
+    for i in sel:
+        temp += i.price
+        print(i.date_add, i.price)
+    print('Всреднем... вы пробухали -', temp/len(sel))
+    print(temp, '=====', temp/len(sel))
